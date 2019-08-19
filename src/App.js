fc470219aca19e2d02a05511e49288b63da799ed
Nevindry <puissant.etienne@gmail.com>
2019-08-19 19:20:44 +0200

    Added iphone alert dialog to turn on switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Metronome from './metronome/Metronome';
+import MobileDialog from './dialog/MobileDialog'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Metronome />
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    let iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+    let dialog;
+    if(iOS){
+      dialog = (<MobileDialog />);
+    }
+
+    return (
+      <div className="App">
+        {dialog}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Metronome />
+      </div>
+    );
+  }
 }
 
 export default App;
