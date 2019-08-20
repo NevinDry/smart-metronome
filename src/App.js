98aa46af2fadba1a1d6957b4690e31057eb2aba1
Nevindry <puissant.etienne@gmail.com>
2019-08-20 02:09:19 +0200

    Added ServiceWorker interaction on update available

--- src/App.js
+++ src/App.js
@@ -3,25 +3,46 @@ import logo from './logo.svg';
 import './App.scss';
 import Metronome from './metronome/Metronome';
 import MobileDialog from './dialog/MobileDialog'
+import MobileToast from './toast/MobileToast'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      updateToast: false,
+    };
+  }
 
+  componentDidMount = () => {
+    // Listening to ServiceWorker event for PWA update integration
+    window.addEventListener('update', (e) => {
+      this.setState({ updateToast: true });
+    }, false);
+  }
 
   render() {
     let iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
     let dialog;
-    if(iOS){
+    if (iOS) {
       dialog = (<MobileDialog />);
     }
 
+
+
     return (
-      <div className="App">
-        {dialog}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Metronome />
+      <div>
+        <div className="App">
+          {dialog}
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Metronome />
+        </div>
+        <MobileToast show={this.state.updateToast} />
+
       </div>
+
     );
   }
 }
