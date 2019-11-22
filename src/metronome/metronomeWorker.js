ad57a1331a45c7c012d3d8f0bb6f2571bffcd6e0
Nevindry <nevin16@hotmail.fr>
2019-11-22 01:38:09 +0100

    Simplified web worker

--- src/metronome/metronomeWorker.js
+++ src/metronome/metronomeWorker.js
@@ -1,7 +1,7 @@
 export default () => {
 
     var timerID = null;
-    var interval = 100;
+    var interval = null;
 
     self.addEventListener('message', e => { /* eslint-disable-line no-restricted-globals */
         if (e.data === "start") {
@@ -9,13 +9,7 @@ export default () => {
                 postMessage("click");
             }, interval)
         } else if (e.data.interval) {
-            interval = e.data.interval;
-            if (timerID) {
-                clearInterval(timerID);
-                timerID = setInterval(function () {
-                    postMessage("click");
-                }, interval)
-            }
+            interval = e.data.interval;         
         } else if (e.data === "stop") {
             clearInterval(timerID);
             timerID = null;
